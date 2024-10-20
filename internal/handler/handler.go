95917ae4881624cd15e0398b74365c9267539987
thuanhphan <phananhthu9333@gmail.com>
2024-10-21 04:49:37 +0700

    FM-3

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,23 +1,41 @@
 package handler
 
 import (
+	"encoding/json"
+	"friend-management-go/internal/model"
 	"friend-management-go/internal/service"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
-type friendshipHandler struct {
+type FriendshipHandler struct {
 	service service.IFriendshipService
 }
 
-func NewAlbumHandler(ms service.IFriendshipService) *friendshipHandler {
-	return &friendshipHandler{service: ms}
+func NewFrienshipHandler(service service.IFriendshipService) *FriendshipHandler {
+	return &FriendshipHandler{service: service}
 }
 
-func (fh friendshipHandler) handleListFriends(w http.ResponseWriter, r *http.Request) {
-	// friends, err := fh.service.GetFriends()
-	// if err != nil {
-	// 	http.Error(w, "Unable to get all movies", http.StatusInternalServerError)
-	// 	return
-	// }
-	// render.RenderList(w, r, friends)
+func (h *FriendshipHandler) CreateFriendship(w http.ResponseWriter, r *http.Request) {
+	var friendship model.Friendship
+	if err := json.NewDecoder(r.Body).Decode(&friendship); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := h.service.MakeFriend(friendship); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *FriendshipHandler) GetFriendsList(w http.ResponseWriter, r *http.Request) {
+	email := chi.URLParam(r, "email")
+	friends, err := h.service.GetFriends(email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(friends)
 }
