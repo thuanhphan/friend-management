aba7e13915d98d8e84362e92ed22adcbb408541a
len.ho <thcl963c@spgroup.com.sg>
2024-10-22 16:41:27 +0700

    FM-5,6,7

--- internal/repository/friendship.go
+++ internal/repository/friendship.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type PostgresRepository struct {
@@ -18,6 +19,8 @@ type PostgresRepository struct {
 func NewPostgresRepository(db *sql.DB) *PostgresRepository {
 	return &PostgresRepository{db: db}
 }
+
+const SUBCRIBED_FRIENDSHIP_STATUS string = "subcribed"
 
 func (p *PostgresRepository) MakeFriend(friendship model.Friendship) error {
 	friend := models.Friendship{
@@ -34,23 +37,105 @@ func (p *PostgresRepository) MakeFriend(friendship model.Friendship) error {
 	return nil
 }
 
-func (p *PostgresRepository) GetFriends(email string) ([]model.User, error) {
-	var friends []model.User
+func (p *PostgresRepository) GetFriends(email string) ([]string, error) {
+	var friends []string
 
 	// Custom SQL query to join users and friendships tables
-	query := `Select email from friendships join users on friendships.user_email = users.email
-			Where friend_email = $1 and status = 'Friend'
-			Union
-			Select friend_email
-			From friendships
-			Where user_email = $1 and status = 'Friend'`
+	query := `Select email from friendships 
+	    Join users on friendships.user_email = users.email
+		Where friend_email = $1 and status = 'friend'
+		Union
+		Select friend_email
+		From friendships
+		Where user_email = $1 and status = 'friend'`
 
 	// Execute the query and bind the results to the friends slice
 	ctx := context.Background()
-	_, err := p.db.ExecContext(ctx, query, email)
+	rows, err := p.db.QueryContext(ctx, query, email)
 
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var friendEmail string
+		if err := rows.Scan(&friendEmail); err != nil {
+			return nil, err
+		}
+		friends = append(friends, friendEmail)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return friends, nil
+}
+
+func (p *PostgresRepository) GetCommonFriends(email1, email2 string) ([]string, error) {
+	var friends []string
+
+	query := `Select email from 
+	    (Select user_email from friendships where friend_email = $1
+		Union 
+		Select friend_email from friendships where user_email = $1) as list1
+		Inner Join
+		(Select user_email from friendships where friend_email = $2
+		Union 
+		Select friend_email from friendships where user_email = $2) as list2
+		on list1.user_email = list2.user_email
+		Inner Join users on users.email = list1.user_email`
+
+	ctx := context.Background()
+	rows, err := p.db.QueryContext(ctx, query, email1, email2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var friendEmail string
+		if err := rows.Scan(&friendEmail); err != nil {
+			return nil, err
+		}
+		friends = append(friends, friendEmail)
+	}
+
+	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 	return friends, nil
+}
+
+func (p *PostgresRepository) UpdateFriendshipStatus(friendship model.Friendship) error {
+	friend := models.Friendship{
+		UserEmail:   null.StringFrom(friendship.UserEmail),
+		FriendEmail: null.StringFrom(friendship.FriendEmail),
+		Status:      null.StringFrom(friendship.Status),
+	}
+
+	ctx := context.Background()
+	// Check if the friendship already exists
+	existingFriendship, err := models.Friendships(
+		qm.Where("user_email = ? AND friend_email = ?", friend.UserEmail, friend.FriendEmail),
+	).One(ctx, p.db)
+
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	if existingFriendship != nil {
+		existingFriendship.Status = null.StringFrom(friend.Status.String)
+		_, err = existingFriendship.Update(ctx, p.db, boil.Infer())
+		return err
+	}
+
+	newFriendship := &models.Friendship{
+		UserEmail:   null.StringFrom(friendship.UserEmail),
+		FriendEmail: null.StringFrom(friendship.FriendEmail),
+		Status:      null.StringFrom(friendship.Status),
+	}
+	return newFriendship.Insert(ctx, p.db, boil.Infer())
 }
