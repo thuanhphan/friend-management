667e8256a5144abb5d3850859ef37b6c654f8478
thu.phan <thu.phan@s3corp.com.vn>
2024-10-28 16:20:46 +0700

    FM-3

--- internal/repository/friendship.go
+++ internal/repository/friendship.go
@@ -1,41 +1,57 @@
 package repository
 
 import (
+	"context"
+	"database/sql"
 	"friend-management-go/internal/model"
+	"friend-management-go/internal/repository/models"
 	"log"
+
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
-func (p *PostgresRepository) MakeFriend(friendship model.Friendship) error {
-	// check connection
-	error := p.connect()
-	if error != nil {
-		log.Fatal(error)
-	}
-	defer p.close()
+type PostgresRepository struct {
+	db *sql.DB
+}
 
-	// model := models.Friendship{
-	// 	UserEmail:   null.StringFrom(friendship.FriendEmail),
-	// 	FriendEmail: null.StringFrom(friendship.FriendEmail),
-	// 	Status:      null.StringFrom(friendship.Status),
-	// }
-	// // error = model.Insert(context.Background(), p.db, boil.Infer())
-	// // if error != nil {
-	// // 	return error
-	// // }
+func NewPostgresRepository(db *sql.DB) *PostgresRepository {
+	return &PostgresRepository{db: db}
+}
+
+func (p *PostgresRepository) MakeFriend(friendship model.Friendship) error {
+	friend := models.Friendship{
+		UserEmail:   null.StringFrom(friendship.FriendEmail),
+		FriendEmail: null.StringFrom(friendship.FriendEmail),
+		Status:      null.StringFrom(friendship.Status),
+	}
+	error := friend.Insert(context.Background(), p.db, boil.Infer())
+	if error != nil {
+		return error
+	}
 
 	log.Println("New friend inserted successfully!")
 	return nil
 }
 
-func (p *PostgresRepository) GetFriends(user model.User) error {
-	// check connection
-	error := p.connect()
-	if error != nil {
-		log.Fatal(error)
+func (p *PostgresRepository) GetFriends(email string) ([]model.User, error) {
+	friends, err := models.Friendships(qm.Where("user_email = ? AND status = ?", email, "friends")).All(context.Background(), p.db)
+	if err != nil {
+		return nil, err
 	}
-	defer p.close()
-
-	//Todos
-
-	return nil
+	var users []model.User
+	for _, friend := range friends {
+		if friend.FriendEmail.Valid {
+			user, err := models.Users(qm.Where("email = ?", friend.FriendEmail)).One(context.Background(), p.db)
+			if err != nil {
+				return nil, err
+			}
+			users = append(users, model.User{
+				Email: user.Email,
+				Name:  user.Name.String,
+			})
+		}
+	}
+	return users, nil
 }
--- internal/repository/new.go
+++ internal/repository/new.go
@@ -5,12 +5,11 @@ import (
 )
 
 type IFriendshipRepository interface {
-	MakeFriend(friendship *model.Friendship) error
+	MakeFriend(friendship model.Friendship) error
 	GetFriends(email string) ([]model.User, error)
-	GetCommonFriends(email1 string, email2 string) ([]*model.User, error)
-	GetReceivableUpdates(email string) ([]*model.User, error)
-	Subcribe(email1 string, email2 string) error
-	Block(email1 string, email2 string) error
-	CreateUser(user model.User) error
-	GetUserByEmail(email string) (model.User, error)
+	// GetCommonFriends(email1, email2 string) ([]model.User, error)
+	// GetReceivableUpdates(email string) (]model.User, error)
+	// UpdateFriendshiptstaus(friendship model.Friendship[) error
+	// CreateUser(user model.User) error
+	// GetUserByEmail(email string) (model.User, error)
 }
